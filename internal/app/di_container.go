fce545c53249efff5de9d58fab8e5101965699fa
Evgeniy <Z_shevelev@mail.ru>
2024-07-29 03:33:04 +0300

    add kafka client

--- internal/app/di_container.go
+++ internal/app/di_container.go
@@ -5,15 +5,17 @@ import (
 	"os"
 
 	"github.com/ShevelevEvgeniy/kafkaManager/config"
+	"github.com/ShevelevEvgeniy/kafkaManager/internal/clients/kafka"
 	dbConn "github.com/ShevelevEvgeniy/kafkaManager/internal/postgres/db_connection"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
 
 type DiContainer struct {
-	cfg *config.Config
-	log *zap.Logger
-	db  *pgxpool.Pool
+	cfg   *config.Config
+	log   *zap.Logger
+	db    *pgxpool.Pool
+	kafka *kafka.Kafka
 }
 
 func NewDiContainer(cfg *config.Config, log *zap.Logger) DiContainer {
@@ -36,4 +38,17 @@ func (di *DiContainer) DB(ctx context.Context) *pgxpool.Pool {
 	}
 
 	return di.db
+}
+
+func (di *DiContainer) Kafka(_ context.Context) *kafka.Kafka {
+	if di.kafka == nil {
+		client, err := kafka.NewKafkaClient(di.cfg.Kafka, di.log)
+		if err != nil {
+			os.Exit(1)
+		}
+
+		di.kafka = client
+	}
+
+	return di.kafka
 }
